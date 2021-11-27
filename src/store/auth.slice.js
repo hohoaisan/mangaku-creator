55010d3afc6a30bac88ee60db0d0e97233976cd4
unknown <hohoaisan@gmail.com>
2021-11-27 12:08:39 +0700

    Auth slice and redux store

---
+++ src/store/auth.slice.js
@@ -0,0 +1,60 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+import * as authAPI from 'apis/auth';
+import * as profileAPI from 'apis/profile';
+
+const initialState = {
+  status: 'idle', // idle | loading
+  isLoggedIn: false,
+  user: null
+  // user : {
+  //   id: '',
+  //   name: '',
+  //   email: '',
+  //   role: '', // user | author | moderator | admin
+  //   emailVerified: false
+  // }
+};
+
+export const login = createAsyncThunk('auth/login', async (credentials) => authAPI.login(credentials));
+export const logout = createAsyncThunk('auth/logout', async ({ refreshToken }) => authAPI.logout({ refreshToken }));
+export const getProfile = createAsyncThunk('auth/getProfile', async ({ accessToken }) => profileAPI.getProfile({ accessToken }));
+
+export const counterSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.isLoggedIn = true;
+        state.user = action.payload?.user;
+      })
+      .addCase(login.rejected, (state) => {
+        state.status = 'idle';
+        state.isLoggedIn = false;
+        state.user = null;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.status = 'idle';
+        state.isLoggedIn = false;
+        state.user = null;
+      })
+      .addCase(logout.rejected, (state) => {
+        state.status = 'idle';
+        state.isLoggedIn = false;
+        state.user = null;
+      })
+      .addCase(getProfile.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.isLoggedIn = true;
+        state.user = action.payload;
+      });
+  }
+});
+
+export default counterSlice.reducer;
