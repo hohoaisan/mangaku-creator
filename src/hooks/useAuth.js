afdd187661427f33236b7293ad5944f10ff7afb9
unknown <hohoaisan@gmail.com>
2021-11-28 16:16:44 +0700

    move useAuth check to protected route

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,42 +1,9 @@
-import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 
-const defaultOptions = {
-  redirectIfLoggedIn: false,
-  redirectIfLoggedOut: true,
-  redirectIfLackPermission: false,
-  redirect: '#',
-  permissions: []
-};
-
-const useAuth = (options = defaultOptions) => {
-  options = Object.assign(defaultOptions, options);
-  const { redirectIfLoggedIn, redirect, redirectIfLoggedOut, redirectIfLackPermission, permissions } = options;
-  const navigate = useNavigate();
+const useAuth = (permissions = []) => {
   const auth = useSelector((state) => state.auth);
   const userPerms = auth?.user?.permissions || [];
   const isLackPermissions = permissions ? !permissions.every((perm) => userPerms.includes(perm)) : false;
-  useEffect(() => {
-    if (auth.isLoggedIn && redirectIfLoggedIn) {
-      navigate(redirect);
-    }
-    if (auth.isLoggedIn && redirectIfLackPermission && isLackPermissions) {
-      navigate(redirect);
-    }
-    if (!auth.isLoggedIn && redirectIfLoggedOut) {
-      navigate(redirect);
-    }
-  }, [
-    auth.isLoggedIn,
-    auth.user,
-    isLackPermissions,
-    navigate,
-    redirect,
-    redirectIfLackPermission,
-    redirectIfLoggedIn,
-    redirectIfLoggedOut
-  ]);
   return {
     isLackPermissions,
     ...auth
