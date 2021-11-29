666b119218b7df3c46940194f3d18c788bc683a5
unknown <hohoaisan@gmail.com>
2021-11-29 22:55:36 +0700

    api must return result instead of axios

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -2,32 +2,35 @@ import axios from './_axios/axios';
 import pick from 'utils/pick';
 import * as AuthAPI from './_endpoints/auth';
 
-export const refreshAccessToken = (props, AxiosOptions) => {
+export const refreshAccessToken = async (props, AxiosOptions) => {
   const data = pick(props, ['refreshToken']);
-  return axios({
+  const result = axios({
     ...AxiosOptions,
     method: 'post',
     url: AuthAPI.TOKEN_REFRESH,
     data
   }).then((res) => res.data);
+  return result;
 };
 
-export const login = (props, AxiosOptions) => {
+export const login = async (props, AxiosOptions) => {
   const data = pick(props, ['email', 'password']);
-  return axios({
+  const result = await axios({
     ...AxiosOptions,
     method: 'post',
     url: AuthAPI.LOGIN,
     data
   }).then((res) => res.data);
+  return result;
 };
 
-export const logout = (props, AxiosOptions) => {
+export const logout = async (props, AxiosOptions) => {
   const data = pick(props, ['refreshToken']);
-  return axios({
+  const result = await axios({
     ...AxiosOptions,
     method: 'post',
     url: AuthAPI.LOGOUT,
     data
   }).then((res) => res.data);
+  return result;
 };
--- src/apis/profile.js
+++ src/apis/profile.js
@@ -2,8 +2,10 @@
 import axios from './_axios/axios';
 import * as ProfileAPI from './_endpoints/profile';
 
-export const getProfile = () =>
-  axios({
+export const getProfile = async () => {
+  const result = await axios({
     method: 'get',
     url: ProfileAPI.PROFILE
   }).then((res) => res.data);
+  return result;
+};
