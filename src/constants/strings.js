353950f29f4a6488c4b279966a4515e5ecdeae89
unknown <hohoaisan@gmail.com>
2022-02-14 09:20:37 +0700

    Vietnamese language

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -2,249 +2,249 @@ const strings = {
   title: 'Managaku',
   common: {
     approvalStatus: {
-      pending: 'Pending',
-      approved: 'Approved',
-      rejected: 'Rejected'
+      pending: 'Chờ duyệt',
+      approved: 'Đã duyệt',
+      rejected: 'Đã từ chối'
     },
     tabs: {
-      default: 'Default',
-      deleted: 'Deleted',
-      pending: 'Pending',
-      approved: 'Approved',
-      rejected: 'Rejected'
+      default: 'Mặc định',
+      deleted: 'Đã xoá',
+      pending: 'Chờ duyệt',
+      approved: 'Đã duyệt',
+      rejected: 'Đã từ chối'
     },
     columns: {
-      name: 'Name',
-      description: 'Description',
-      authorUser: 'Author user',
-      approvalStatus: 'Approval status',
-      restricted: 'Restricted',
-      createdAt: 'Created At',
-      deletedAt: 'Deleted At',
-      updatedAt: 'Last modified',
-      uniqueKey: 'Unique key'
+      name: 'Tên',
+      description: 'Mô tả',
+      authorUser: 'Tác giả',
+      approvalStatus: 'Tình trạng duyệt',
+      restricted: 'Giới hạn',
+      createdAt: 'Thời điểm tạo',
+      deletedAt: 'Thời điểm xoá',
+      updatedAt: 'Thời điểm cập nhật',
+      uniqueKey: 'Key duy nhất'
     }
   },
   toast: {
-    signedin: 'Logged in',
-    expireSignOut: 'Token expired, logged out',
-    signOut: 'Logged out'
+    signedin: 'Đã đăng nhập',
+    expireSignOut: 'Token hết hạn, đăng xuất',
+    signOut: 'Đã đăng xuất'
   },
   menus: {
-    people: 'People',
-    manageUsers: 'Manage Users',
-    comic: 'Manage Comics',
-    manageComic: 'Comics',
-    manageAuthor: 'Authors',
-    manageFormat: 'Formats',
-    manageGenre: 'Genres'
+    people: 'Người dùng',
+    manageUsers: 'Quản lí người dùng',
+    comic: 'Quản lí truyện',
+    manageComic: 'Truyện',
+    manageAuthor: 'Tác giả',
+    manageFormat: 'Định dạng',
+    manageGenre: 'Thể loại'
   },
 
   entities: {
     comic: {
-      views: 'Views',
-      ratings: 'Ratings',
-      favorites: 'Favorites',
-      approval: 'Approval',
-      authors: 'Authors',
-      formats: 'Formats',
-      genres: 'Genres',
-      description: 'Description',
-      chapters: 'Chapters',
-      noChap: 'No chapter',
-      deletedAt: 'Deleted at',
-      createdAt: 'Created at',
-      updatedAt: 'Updated at'
+      views: 'Lượt xem',
+      ratings: 'Đánh giá',
+      favorites: 'Lượt yêu thích',
+      approval: 'Tình trạng duyệt',
+      authors: 'Tác giả',
+      formats: 'Định dạng',
+      genres: 'Thể loại',
+      description: 'Mô tả',
+      chapters: 'Chương truyện',
+      noChap: 'Không có chương nào',
+      createdAt: 'Thời điểm tạo',
+      deletedAt: 'Thời điểm xoá',
+      updatedAt: 'Thời điểm cập nhật'
     },
     chapter: {
-      number: 'Chapter',
-      volume: 'Volume',
-      name: 'Chapter name',
-      numPages: 'Pages'
+      number: 'Chương số',
+      volume: 'Tập (Volume)',
+      name: 'Tên chương',
+      numPages: 'Số trang'
     },
     user: {
-      name: 'Name',
-      role: 'Role',
+      name: 'Tên',
+      role: 'Vai trò',
       email: 'Email',
-      password: 'Password',
-      emailVerified: 'Email verified',
-      restrict: 'Restrict',
-      banned: 'Banned',
-      verified: 'Verified',
-      notVerified: 'Not Verified'
+      password: 'Mật khẩu',
+      emailVerified: 'Email đã xác thực',
+      restrict: 'Giới hạn',
+      banned: 'Bị Ban',
+      verified: 'Đã xác thực',
+      notVerified: 'Chưa xác thực'
     }
   },
 
   buttons: {
-    signin: 'Sign in',
-    register: 'Procceed to register',
-    signout: 'Logout',
-    close: 'Close',
-    create: 'Create',
-    cancel: 'Cancel',
-    confirm: 'Confirm',
-    update: 'Update',
-    uploadNewChapter: 'Upload new chapter',
-    upload: 'Upload'
+    signin: 'Đăng nhập',
+    register: 'Tiến hành đăng kí',
+    signout: 'Đăng xuất',
+    close: 'Đóng',
+    create: 'Tạo mới',
+    cancel: 'Huỷ bỏ',
+    confirm: 'Xác nhận',
+    update: 'Cập nhật',
+    uploadNewChapter: 'Tải lên chương mới',
+    upload: 'Tải lên'
   },
   errors: {
-    unknown: 'Unknow error',
-    network: 'Network error',
-    server: 'Server error'
+    unknown: 'Lỗi không xác định',
+    network: 'Lỗi mạng',
+    server: 'Lỗi máy chủ'
   },
   roles: {
-    ADMIN: 'Administrator',
-    MOD: 'Moderator',
-    AUTHOR: 'Author',
-    USER: 'User'
+    ADMIN: 'Quản trị viên',
+    MOD: 'Biên tập viên',
+    AUTHOR: 'Tác giả',
+    USER: 'Người dùng'
   },
   forms: {
     labels: {
       email: 'Email',
-      name: 'Name',
-      password: 'Password',
-      authorName: 'Author name',
-      authorDescription: 'Author Description',
-      restrict: 'Restrict',
-      approvalStatus: 'Approval status',
-      key: 'Unique Key',
-      description: 'Description',
-      title: 'Title',
-      author: 'Author',
-      genre: 'Genres',
-      format: 'Formats',
-      covers: 'Covers',
-      approval: 'Approval Status',
-      chapNumber: 'Number',
-      volume: 'Volume',
-      pages: 'Pages'
+      name: 'Tên',
+      password: 'Mật khẩu',
+      authorName: 'Tên tác giả',
+      authorDescription: 'Mô tả tác giả',
+      restrict: 'Giới hạn',
+      approvalStatus: 'Tình trạng duyệt',
+      key: 'Key duy nhất',
+      description: 'Mô tả',
+      title: 'Tiêu đề',
+      author: 'Tác giả',
+      genre: 'Thể loại',
+      format: 'Định dạng',
+      covers: 'Ảnh bìa',
+      approval: 'Tình trạng duyệt',
+      chapNumber: 'Chương số',
+      volume: 'Tập (Volume)',
+      pages: 'Trang'
     },
     validations: {
-      emailValid: 'Must be a valid email',
-      emailRequired: 'Email is required',
-      passwordRequired: 'Password is required',
-      nameRequired: 'Name is required',
-      descriptionRequired: 'Description is required',
-      keyRequired: 'Key is required',
-      titleRequired: 'Title is required',
-      authorRequired: 'Author is required',
-      genreRequired: 'Genre is required',
-      formatRequired: 'Format is required',
-      coverImageRequired: 'Cover image is required',
-      defaultCoverRequired: 'A default cover is required',
-      chapNumberRequired: 'Chapter number is required',
-      chapPagesRequired: 'Chapter pages is required'
+      emailValid: 'Phải là email hợp lệ',
+      emailRequired: 'Phải nhập email',
+      passwordRequired: 'Phải nhập mật khẩu',
+      nameRequired: 'Phải nhập tên',
+      descriptionRequired: 'Phải nhập mô tả',
+      keyRequired: 'Phải nhập Key',
+      titleRequired: 'Phải nhập tiêu đề',
+      authorRequired: 'Phải chọn tác giả',
+      genreRequired: 'Phải chọn thể loại',
+      formatRequired: 'Phải chọn định dạng',
+      coverImageRequired: 'Phải có ảnh bìa',
+      defaultCoverRequired: 'Phải có ảnh bìa mặc định',
+      chapNumberRequired: 'Phải nhập số của chương',
+      chapPagesRequired: 'Phải tải lên trang của chương'
     }
   },
   pages: {
     auth: {
-      singinWithEmail: 'Sign in with Email address',
-      greet1: 'Hi, Welcome Back',
-      greet2: ' Enter your credentials to continue',
-      noAccount: 'Don&apos;t have an account?',
-      becomeAuthor: 'Become an author',
-      pendingAuthor: 'You have registered to become an author, but was not approved yet, please wait',
-      acceptedAuthor: 'You have become an author!, Please logout and login again!',
-      rejectedAuthor: 'Your request to become an author was rejected'
+      singinWithEmail: 'Đăng nhập với email và mật khẩu',
+      greet1: 'Chào mừng trở lại',
+      greet2: 'Vui lòng nhập thông tin đăng nhập để tiếp tục',
+      noAccount: 'Chưa có tài khoản',
+      becomeAuthor: 'Trở thành tác giả',
+      pendingAuthor: 'Bạn đã đăng kí trở thành tác giả, nhưng chưa được duyệt, hãy chờ',
+      acceptedAuthor: 'Bạn đã trở thành tác giả, vui lòng đăng xuất và đăng nhập lại',
+      rejectedAuthor: 'Yêu cầu trở thành tác giả đã bị từ chối'
     },
     author: {
-      manage: 'Manage Authors',
-      create: 'Create Author',
-      delete: 'Delete Author',
-      update: 'Update Author',
-      restore: 'Restore Author',
+      manage: 'Quản lí tác giả',
+      create: 'Thêm tác giả',
+      delete: 'Xoá tác giả',
+      update: 'Cập nhật tác giả',
+      restore: 'Khôi phục tác giả',
       mutations: {
-        createSuccess: 'Author created',
-        deleteSuccess: 'Deleted author',
-        updateSuccess: 'Updated author',
-        restoreSuccess: 'Restored author',
-        restoreFailed: 'Unable to restore author'
+        createSuccess: 'Đã thêm tác giả',
+        deleteSuccess: 'Đã xoá tác giả',
+        updateSuccess: 'Đã cập nhật tác giả',
+        restoreSuccess: 'Đã khôi phục tác giả',
+        restoreFailed: 'Không thể khôi phục tác giả'
       },
       prompt: {
-        deleteAuthor: 'Are you sure want to delete this author',
-        restoreAuthor: 'Are you sure want to restore this author'
+        deleteAuthor: 'Bạn có chắc chắn muốn xoá tác giả này không',
+        restoreAuthor: 'Bạn có chắc chắn muốn khôi phục tác giả này không'
       }
     },
     genre: {
-      manage: 'Manage Genres',
-      create: 'Create Genre',
-      delete: 'Delete Genre',
-      update: 'Update Genre',
+      manage: 'Quản lí thể loại',
+      create: 'Tạo thể loại',
+      delete: 'Xoá thể loại',
+      update: 'Cập nhật thể loại',
       mutations: {
-        createSuccess: 'Genre created',
-        deleteSuccess: 'Deleted genre',
-        updateSuccess: 'Updated genre'
+        createSuccess: 'Đã tạo thể loại',
+        deleteSuccess: 'Đã xoá thể loại',
+        updateSuccess: 'Đã cập nhật thể loại'
       },
       prompt: {
-        deleteAuthor: 'Are you sure want to delete this genre'
+        deleteAuthor: 'Bạn có chắc chắn muốn xoá thể loại này không'
       }
     },
     format: {
-      manage: 'Manage Formats',
-      create: 'Create Format',
-      delete: 'Delete Format',
-      update: 'Update Format',
+      manage: 'Quản lí định dạng',
+      create: 'Tạo định dạng',
+      delete: 'Xoá định dạng',
+      update: 'Cập nhật định dạng',
       mutations: {
-        createSuccess: 'Format created',
-        deleteSuccess: 'Deleted format',
-        updateSuccess: 'Updated format'
+        createSuccess: 'Đã tạo định dạng',
+        deleteSuccess: 'Đã xoá định dạng',
+        updateSuccess: 'Đã cập nhật định dạng'
       },
       prompt: {
-        deleteFormat: 'Are you sure want to delete this format'
+        deleteFormat: 'Bạn có chắc chắn muốn xoá định dạng này không'
       }
     },
     comic: {
-      mangage: 'Manage Comics',
-      detail: 'Comic $title',
-      create: 'Add new comic',
-      delete: 'Delete Comic',
-      restore: 'Restore Comic',
-      update: 'Update Comic',
+      mangage: 'Quản lí truyện',
+      detail: 'Truyện $title',
+      create: 'Thêm truyện mới',
+      delete: 'Xoá truyện',
+      restore: 'Khôi phục truyện',
+      update: 'Cập nhật truyện',
       mutations: {
-        createSuccess: 'Comic created',
-        deleteSuccess: 'Deleted comic',
-        updateSuccess: 'Updated comic',
-        restoreSuccess: 'Restored comic',
-        restoreFailed: 'Unable to restore comic'
+        createSuccess: 'Truyện đã được thêm',
+        deleteSuccess: 'Đã xoá truyện',
+        updateSuccess: 'Đã cập nhật truyện',
+        restoreSuccess: 'Đã khôi phục truyện',
+        restoreFailed: 'Chưa thể khôi phục truyện'
       },
       prompt: {
-        deleteComic: 'Are you sure want to delete this comic',
-        restoreComic: 'Are you sure want to restore this comic'
+        deleteComic: 'Bạn có chắc chắn muốn xoá truyện này không',
+        restoreComic: 'Bạn có chắc chắn muốn khôi phục truyện này không'
       }
     },
     chapter: {
-      upload: 'Upload chapter',
-      update: 'Update chapter',
-      restore: 'Restore chapter',
-      delete: 'Delete chapter',
+      upload: 'Tải chương truyện',
+      update: 'Cập nhật chương truyện',
+      restore: 'Khôi phục chương truyện',
+      delete: 'Xoá chương truyện',
       mutations: {
-        uploadSuccess: 'Chapter uploaded',
-        updateSuccess: 'Chapter uploaded',
-        restoreFailed: 'Unable to restore chapter',
-        restoreSuccess: 'Restored chapter',
-        deleteSuccess: 'Deleted chapter'
+        uploadSuccess: 'Đã tải lên chương truyện',
+        updateSuccess: 'Đã cập nhật chương truyện',
+        restoreFailed: 'Có lỗi khi khôi phục chương truyện',
+        restoreSuccess: 'Đã khôi phục chương truyện',
+        deleteSuccess: 'Đã xoá chương truyện'
       },
       prompt: {
-        restoreChapter: 'Are you sure want to restore this chapter',
-        deleteChapter: 'Are you sure want to delete this chapter'
+        restoreChapter: 'Bạn có chắc chắn muốn khôi phục chương truyện này không',
+        deleteChapter: 'Bạn có chắc chắn muốn xoá chương truyện này không'
       }
     },
     user: {
-      manage: 'Manage users',
-      create: 'Create user',
-      delete: 'Delete user',
-      restore: 'Restore user',
+      manage: 'Quản lí người dùng',
+      create: 'Thêm người dùng',
+      delete: 'Xoá người dùng',
+      restore: 'Khôi phục người dùng',
       mutations: {
-        createSuccess: 'User created',
-        deleteSuccess: 'Deleted user',
-        deleteFailed: 'Unable to delete user',
-        updateSuccess: 'User updated',
-        restoreSuccess: 'Restored user',
-        restoreFailed: 'Unable to restore user'
+        createSuccess: 'Đã tạo mới người dùng',
+        deleteSuccess: 'Đã xoá người dùng',
+        deleteFailed: 'Không thể xoá người dùng',
+        updateSuccess: 'Đã cập nhật người dùng',
+        restoreSuccess: 'Đã khôi phục người dùng',
+        restoreFailed: 'Không thể khôi phục người dùng'
       },
       prompt: {
-        deleteUser: 'Are you sure want to delete this user',
-        restoreUser: 'Are you sure want to restore this user'
+        deleteUser: 'Bạn có chắc chắn muốn xoá người dùng này không',
+        restoreUser: 'Bạn có chắc chắn muốn khôi phục người dùng này không'
       }
     }
   }
